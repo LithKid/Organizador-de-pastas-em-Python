44e2c347c23ecedea3f7de9f14f7c3af5e881edc
Daniel Isaias de Oliveira dos Santos <daniel21iasas@gmail.com>
2026-02-23 13:51:32 -0300

    adicionando README e gitignore

--- untidled.ps1
+++
@@ -1,8 +0,0 @@
-$wshell = New-Object -ComObject WScript.Shell
-
-$wshell.Popup("Esse curso eh muito legal")
-
-$wshell.Run("notepad")# Coloca o foco na janela do Notepad
-$wshell.AppActivate("Notepad.exe")# Coloca o foco na janela do Notepad
-Start-Sleep 1
-$wshell.SendKeys("Curso de PowerShell - A melhor linguagem de script do mundo!")# Envia texto para o Notepad   
